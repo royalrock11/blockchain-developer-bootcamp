49fa05de7deb24136b48b90d365cab6427998c80
royalrock11 <rdp@DESKTOP-TJNKQEG.localdomain>
2023-04-01 14:40:17 -0400

    5 Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract Token {
 	string public name; 	
@@ -9,10 +9,37 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+	mapping(address => uint256) public balanceOf;
+	
+	event Transfer(address indexed from,
+	address indexed to, 
+	uint256 value
+	); 
+
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply
+	) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
+	}
+
+	function transfer(address _to, uint256 _value) 
+	public 
+	returns (bool success) 
+	{
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
+
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(msg.sender, _to, _value);
+
+		return true;
 	}
 }
  
