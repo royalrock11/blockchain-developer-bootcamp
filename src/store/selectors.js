fd73f4dc6005a5e6b817653a9e2091643db83c87
royalrock11 <rdp@DESKTOP-TJNKQEG.localdomain>
2023-05-01 13:44:03 -0400

    25. Price chart

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect'
-import { get, groupBy, reject } from 'lodash';
+import { get, groupBy, reject, maxBy, minBy } from 'lodash';
 import moment from 'moment'
 import { ethers } from 'ethers';
 
@@ -32,12 +32,12 @@ const decorateOrder = (order, tokens) => {
   let token0Amount, token1Amount
 
   // Note: DApp should be considered token0, mETH is considered token1
-  // Example: Giving mETH in exchange for DApp
+  // Example: Giving mETH in exchange for Prince
   if (order.tokenGive === tokens[1].address) {
-    token0Amount = order.amountGive // The amount of DApp we are giving
+    token0Amount = order.amountGive // The amount of Prince we are giving
     token1Amount = order.amountGet // The amount of mETH we want...
   } else {
-    token0Amount = order.amountGet // The amount of DApp we want
+    token0Amount = order.amountGet // The amount of Prince we want
     token1Amount = order.amountGive // The amount of mETH we are giving...
   }
 
@@ -115,4 +115,68 @@ const decorateOrderBookOrder = (order, tokens) => {
     orderTypeClass: (orderType === 'buy' ? GREEN : RED),
     orderFillAction: (orderType === 'buy' ? 'sell' : 'buy')
   })
+}
+
+//--------------------------------------------------------------------------------------------
+// PRICE CHART
+
+export const priceChartSelector = createSelector(
+  filledOrders,
+  tokens,
+  (orders, tokens) => {
+    if (!tokens[0] || !tokens[1]) { return }
+
+    // Filter orders by selected tokens
+    orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+    orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+    // Sort orders by date ascending to company history
+    orders = orders.sort((a,b) => a.timestamp - b.timestamp)
+
+    orders = orders.map((o) => decorateOrder(o, tokens))
+    
+    // Get last 2 orders for final price & change price
+    let secondLastOrder, lastOrder
+    [secondLastOrder, lastOrder] = orders.slice(orders.lenght - 2, orders.lenght)
+
+    // get last order price
+    const lastPrice = get(lastOrder, 'tokenPrice', 0)
+
+    // get second last order price
+    const secondLastPrice = get(secondLastOrder, 'tokenPrice', 0)
+
+    return ({
+      lastPrice,
+      lastPriceChange: (lastPrice >= secondLastPrice ? '+' : '-'),
+      series: [{
+        data: buildGraphData(orders)
+      }]
+    })
+  }
+)
+const buildGraphData = (orders) => {
+  // Group the orders by hour for the graph
+  orders = groupBy(orders, (o) => moment.unix(o.timestamp).startOf('hour').format())
+
+  // Get each hour where data exists
+  const hours = Object.keys(orders)
+
+  // Build the graph series
+  const graphData = hours.map((hour) => {
+    // Fetch all orders from current hour
+    const group = orders[hour]
+
+    // Calculate price values: open, high, low, close
+    const open = group[0] // first order
+    const high = maxBy(group, 'tokenPrice') // high price
+    const low = minBy(group, 'tokenPrice') // low price
+    const close = group[group.length - 1] // last order
+
+    return({
+      x: new Date(hour),
+      y: [open.tokenPrice, high.tokenPrice, low.tokenPrice, close.tokenPrice]
+    })
+  })
+
+  return graphData
 }
