487d2df729347e308dc2cdab0df214a56dc1da51
royalrock11 <royalrock13@gmail.com>
2023-05-12 10:58:58 -0400

    Latest commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {}, 
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(','),
     }
   },
